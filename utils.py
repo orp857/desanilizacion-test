1946af92f0fd75546b457e51f3fc90e766e432a8
orp857 <82544628+orp857@users.noreply.github.com>
2024-06-05 17:14:39 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,8 +13,8 @@ from langchain.prompts import PromptTemplate
 # Configuración y constantes globales
 load_dotenv()
 
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 MODEL_NAME = "text-embedding-ada-002"
 pinecone.init(api_key=PINECONE_API_KEY, environment='us-east-1')
@@ -37,51 +37,35 @@ def transform_dict_to_document(dict_list):
     document_list = []
     
     for dict_obj in dict_list:
-        # Extraer el contenido de la página y los metadatos del diccionario
         page_content = dict_obj['metadata']['text']
         page_content = page_content.replace('\n', '')  # Elimina los saltos de línea
 
         metadata = {'page': dict_obj['metadata']['page'], 'source': dict_obj['metadata']['source']}
 
-        # Crear un Document con el contenido de la página y los metadatos
         doc = Document(page_content=page_content, metadata=metadata)
-
-        # Añadir el Document a la lista
         document_list.append(doc)
 
-    # Devolver la lista de Documents
     return document_list
 
-  
+def get_similiar_docs_pinecone(query, k=10, score=False):
+    query_embedding = EMBEDDINGS.embed_query(query)
+    result_query = INDEX_PINECONE.query(query_embedding, top_k=k, include_metadata=True)
+    result_query_json = json.dumps(result_query.to_dict())
 
-def get_similiar_docs_pinecone(query,k=10,score=False):
-  import json
-  query_embedding= embeddings.embed_query(query)
-  result_query = index.query(query_embedding, top_k=k, include_metadata=True)
-  result_query_json=json.dumps(result_query.to_dict())
+    def json_to_list(json_string):
+        json_string = json_string.replace("'", '"')  # JSON necesita comillas dobles
+        json_dict = json.loads(json_string)
+        matches_list = json_dict['matches']
+        return matches_list
 
-  def json_to_list(json_string):
-    # Convertir la cadena de caracteres a diccionario
-    json_string = json_string.replace("'", '"')  # JSON necesita comillas dobles
-    json_dict = json.loads(json_string)
-    
-    # Extraer 'matches' que es una lista de diccionarios
-    matches_list = json_dict['matches']
+    similar_docs = transform_dict_to_document(json_to_list(result_query_json))
+    return similar_docs
 
-    return matches_list
-
-  similar_docs=transform_dict_to_document(json_to_list(result_query_json))
-
-  return similar_docs
-
-
-
-# Función para refinar una consulta dada una conversación anterior
 def query_refiner(conversation, query):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"Dada la consulta del usuario y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica centrada en el área de normativas eléctricas. Esta pregunta refinada debe ayudarte a obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible. La consulta refinada debe estar en forma de pregunta y no exceder de 2 oraciones.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-    	temperature=0.3,
+        temperature=0.3,
         max_tokens=512,
         top_p=1,
         frequency_penalty=0,
@@ -89,21 +73,15 @@ def query_refiner(conversation, query):
     )
     return response['choices'][0]['text']
 
-# Función para obtener el historial de conversación
 def get_conversation_string():
     conversation_string = ""
-    for i in range(len(st.session_state['responses'])-1):
-        
-        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    for i in range(len(st.session_state['responses']) - 1):
+        conversation_string += "Human: " + st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: " + st.session_state['responses'][i + 1] + "\n"
     return conversation_string
 
-
-# Función para obtener una respuesta a una consulta
 def get_answer(query):
     similar_docs = get_similiar_docs_pinecone(query)
-    print(similar_docs)
-    print('------')
     answer = qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
     return answer
 
