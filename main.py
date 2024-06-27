65f25be4730603ba00c3a21ab3ab063df61ece98
orp857 <82544628+orp857@users.noreply.github.com>
2024-06-27 15:53:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ st.markdown("<h2 style='text-align: center;'>Asistente Regulatorio Virtual</h2>"
 #st.markdown("<h6 style='text-align: center;'>Creado por: Robinson Cornejo</h6>", unsafe_allow_html=True)
 
 
-# if 'responses' not in st.session_state:
-#     st.session_state['responses'] = ["Hola, soy tu asistente regulatorio virtual, ¿En qué puedo ayudarte hoy?"]
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["Hola, soy tu asistente regulatorio virtual, ¿En qué puedo ayudarte hoy?"]
 
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
